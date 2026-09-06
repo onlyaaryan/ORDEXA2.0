be4373272fa59928fa111f3ed479fc2e77e10ae2
onlyaaryan <144095163+onlyaaryan@users.noreply.github.com>
2026-09-06 19:32:47 +0530

    Update DBConnection.java

--- ORDEXA/WEB-INF/classes/com/queue/DBConnection.java
+++ ORDEXA/WEB-INF/classes/com/queue/DBConnection.java
@@ -2,20 +2,28 @@ package com.queue;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class DBConnection {
-    public static Connection getConnection() {
-        Connection con = null;
+
+    public static Connection getConnection() throws SQLException {
+
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            String url = "jdbc:mysql://mysql-1bb86359-ordexa1.a.aivencloud.com:14191/defaultdb?useSSL=true&requireSSL=true&serverTimezone=UTC";
-            String user = "avnadmin";
-            String password = "AVNS_9T-wEaaSv03_M6knKcf";
-
-            con = DriverManager.getConnection(url, user, password);
-        } catch (Exception e) {
-            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            throw new SQLException("MySQL JDBC Driver not found.", e);
         }
-        return con;
+
+        String url =
+                "jdbc:mysql://mysql-1bb86359-ordexa1.a.aivencloud.com:14191/defaultdb"
+                        + "?sslMode=REQUIRED"
+                        + "&serverTimezone=UTC";
+
+        String user = "avnadmin";
+
+        // Aiven ka NEW password yaha daalo
+        String password = "AVNS_9T-wEaaSv03_M6knKcf";
+
+        return DriverManager.getConnection(url, user, password);
     }
 }
